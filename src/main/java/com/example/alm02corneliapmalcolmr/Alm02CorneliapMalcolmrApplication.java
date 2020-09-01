bac491dfb2fd659d71d3a3a9f662f4552eeb20e8
Cornelia Bea Persson <corneliap@live.se>
2020-09-01 15:00:50 +0200

    Initial commit

---
+++ src/main/java/com/example/alm02corneliapmalcolmr/Alm02CorneliapMalcolmrApplication.java
@@ -0,0 +1,13 @@
+package com.example.alm02corneliapmalcolmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Alm02CorneliapMalcolmrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Alm02CorneliapMalcolmrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alm02corneliapmalcolmr/Alm02CorneliapMalcolmrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alm02corneliapmalcolmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Alm02CorneliapMalcolmrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

cf3f6a3446a6136c08a6d11979a11c8cac9cd799
Cornelia Bea Persson <corneliap@live.se>
2020-09-01 16:07:46 +0200

    created randomizer method for first name

--- src/main/java/com/example/alm02corneliapmalcolmr/RandomFactor.java
+++ src/main/java/com/example/alm02corneliapmalcolmr/RandomFactor.java
@@ -2,11 +2,19 @@ package com.example.alm02corneliapmalcolmr;
 
 public class RandomFactor {
 
-//    TODO:method för förnamn
-//    TODO:method för efternamn
-//    TODO:method för hårfärg
-//    TODO:method för skostorlek
-//    TODO:method för stjärntecken
-//    TODO:method för antal nuvarande partners
+    public String firstNameRandomizer(String input) {
+        String result = "";
+
+        if (input.length() <= 4) {
+//            Gör något
+        } else if (input.length() < 7) {
+//            Gör något
+        } else if (input.length() < 10) {
+//            Gör något
+        } else {
+//            Gör något
+        }
+        return result;
+    }
 
 }

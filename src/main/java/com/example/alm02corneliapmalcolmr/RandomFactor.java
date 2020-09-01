63424c4225fd34534534cd08c8d699ada3fc9145
Valle <valletsf@gmail.com>
2020-09-01 17:42:05 +0200

    Added method: randomStringFromLastName to RandomFactor.java. Also added basic gui

--- src/main/java/com/example/alm02corneliapmalcolmr/RandomFactor.java
+++ src/main/java/com/example/alm02corneliapmalcolmr/RandomFactor.java
@@ -9,5 +9,25 @@ public class RandomFactor {
 //    TODO:method för stjärntecken
 //    TODO:method för antal nuvarande partners
 
+    public String randomStringFromLastName(String lastName){
+        if (lastName.length() >= 10){
+            return "your worst enemy";
+        }
+        else if (lastName.length() >= 8){
+            return "your secret lover";
+        }
+        else if (lastName.length() >= 6 ){
+            return "your neighbors cat";
+        }
+        else if (lastName.length() >= 4 ){
+            return "your prison cellmate";
+        }
+        else if (lastName.length() > 0 ){
+            return "your pole dancing teacher";
+        }
+        else
+            return "you";
+    }
+
 
 }
